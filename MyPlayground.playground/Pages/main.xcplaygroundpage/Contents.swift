5cdebc41dc61e7fe6a4c64a7642bf20399349538
Cristopher Chavez <cristopherchavezny@gmail.com>
2016-09-18 16:35:35 -0400

    mathBoolOp Done

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,84 +14,88 @@
 /*: Question 1
 ### 1. Add two Doubles
 */
-// write your code here
+*/
+var firstDouble = 2.5
+var secontDouble = 3.5
 
-
+let total = firstDouble + secontDouble
+print(total)
 /*: Question 2
 ### 2. Add an Int to a Double
 */
-// write your code here
-
+Double(1) + 3.5
 
 /*: Question 3
 ### 3. Compare two Ints for equality
 */
-// write your code here
-
+2 == 2
 
 /*: Question 4
 ### 4. Compare two Doubles for equality
 */
-// write your code here
-
+2.5 == 7.5
 
 /*: Question 5
 ### 5. Compare an Int and a Double equality
 */
-// write your code here
-
+3 == 3.0
 
 /*: Question 6
 ### 6. Compare 3 and 3.5 for equality (NB what do you have to be mindful of in this case?)
 */
-// write your code here
-
+3 == 3.5
 
 /*: Question 7
 ### 7. Compare 3 and 3.5 for equality and 6.1 and 7 for inequality
 */
-// write your code here
-
+3 != 3.5
+6.1 != 7
 
 /*: Question 8
 ### 8. Write code expressing "if a less than b or x is greater than or equal to y"
 */
-// write your code here
+let a = 2
+let b = 3
+let x = 2
+let y = 2
 
+a < b || x >= y
 
 /*: Question 9
 ### 9. Write a function which returns the sum of three Ints
 */
-// write your code here
-
+func sumOfThreeInts(intOne: Int, intTwo: Int, intthree: Int) -> Int{
+    return intOne + intTwo + intthree
+}
 
 /*: Question 10
 ### 10. Write a function average_i which returns the average of three Ints as an Int
 */
-// write your code here
-
+func averageOfThreeInts(intOne: Int, intTwo: Int, intthree: Int) -> Int{
+    return (intOne + intTwo + intthree) / 3
+}
 
 /*: Question 11
 ### 11. Use the return value of the function written for Question 10 in a boolean expression
 */
-// write your code here
-
+let number = averageOfThreeInts(intOne: 2, intTwo: 4, intthree: 5) > 4
 
 /*: Question 12
 ### 12. Write a function average_f which returns the average of three Ints as a Float
 */
-// write your code here
-
+func sumOfThreeInts(intOne: Int, intTwo: Int, intthree: Int) -> Float{
+    return Float(intOne + intTwo + intthree)
+}
 
 /*: Question 13
 ### 13. Call average_f with values 1, 3, and 5 and test if the result equals 3.0
 */
-// write your code here
-
+sumOfThreeInts(intOne: 1, intTwo: 3, intthree: 5) == 3.0
 
 /*: Question 14
 ### 14. Call average_i with values 1, 3, and 5 and test if the result is both greater than 1 and less than 5.
 */
-// write your code here
+let result = averageOfThreeInts(intOne: 1, intTwo: 3, intthree: 5)
 
-*/
+result > 1 || result < 5
+
