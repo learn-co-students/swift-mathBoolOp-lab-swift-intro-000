8d20d3ad55c4c18bd01e5d57e815fde61414a0d2
Avinash <aviseeram23@gmail.com>
2016-09-22 00:54:21 -0400

    multiline comment was hidden had to delete everything above question #1 in order to make playground work

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -1,97 +1,103 @@
-/*: Outline
- 
- 
- # Switch Statements
- 
- ### Readings associated with this lab
- 
- * [Switch Statements]https://github.com/learn-co-curriculum/swift-switchStatement-readme)
- * [Booleans and Operators](https://github.com/learn-co-curriculum/swift-booleans-readme)
- * [Conditionals](https://github.com/learn-co-curriculum/swift-conditionals-readme)
- 
+ // 1.  Adding Two doubles
 
-
-/*: Question 1
-### 1. Add two Doubles
-*/
-// write your code here
-
-
+let aNum = 2.3
+let bNum = 3.4
+ let sum = a + b
 /*: Question 2
 ### 2. Add an Int to a Double
 */
-// write your code here
+let num = 3
+ let num2 = 3.2
+ 
+ let sumOfNum = num + Int(num2)
 
 
 /*: Question 3
 ### 3. Compare two Ints for equality
 */
-// write your code here
+6 == 1
 
 
 /*: Question 4
 ### 4. Compare two Doubles for equality
 */
-// write your code here
+3.4 == 3.0
 
 
 /*: Question 5
 ### 5. Compare an Int and a Double equality
 */
-// write your code here
+3 == 3.4
 
 
 /*: Question 6
 ### 6. Compare 3 and 3.5 for equality (NB what do you have to be mindful of in this case?)
 */
-// write your code here
+3 == 3.5
 
 
 /*: Question 7
 ### 7. Compare 3 and 3.5 for equality and 6.1 and 7 for inequality
 */
-// write your code here
+3 == 3.5 && 6.1 == 7
 
 
 /*: Question 8
 ### 8. Write code expressing "if a less than b or x is greater than or equal to y"
 */
-// write your code here
+let a = 1
+let b = 5
+let x = 10
+let y = 34
+ 
+ a < b || x > y
 
 
 /*: Question 9
 ### 9. Write a function which returns the sum of three Ints
 */
-// write your code here
+ func sumCalculation(num1: Int, num2: Int, num3: Int) -> Int{
+    return num1 + num2 + num3
+ }
 
-
+sumCalculation(num1: 3, num2: 4, num3: 5)
 /*: Question 10
 ### 10. Write a function average_i which returns the average of three Ints as an Int
 */
-// write your code here
+ func average_i (num1: Int, num2: Int, num3: Int) -> Int{
+    return (num1+num2+num3)/3
+ }
 
-
+average_i(num1: 5, num2: 10, num3: 45)
 /*: Question 11
 ### 11. Use the return value of the function written for Question 10 in a boolean expression
 */
-// write your code here
+average_i(num1: 30, num2: 20, num3: 10) > 1
 
 
 /*: Question 12
 ### 12. Write a function average_f which returns the average of three Ints as a Float
 */
-// write your code here
+ func average_f (num1:Int, num2:Int, num3:Int) -> Float {
+    return Float(num1 + num2 + num3)/3.0
+
+}
+
 
 
 /*: Question 13
 ### 13. Call average_f with values 1, 3, and 5 and test if the result equals 3.0
 */
-// write your code here
+average_f(num1: 1, num2: 3, num3: 5) == 3.0
 
 
 /*: Question 14
 ### 14. Call average_i with values 1, 3, and 5 and test if the result is both greater than 1 and less than 5.
 */
-// write your code here
+let test = average_i(num1: 1, num2: 3, num3: 5)
+    
+   test <= 1 && test >= 5
 
-*/
+/*: Question 1
+ ### 1. Add two Doubles
+ */
