fd7ad2cc84dc21f4f2e41eff6d809b374a658d72
Miguel Granados <miguelgranados@Miguels-MacBook-Pro-2.local>
2016-10-12 20:29:26 -0500

    Otra mas

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,83 +15,128 @@
 ### 1. Add two Doubles
 */
 // write your code here
-
+ 
+ 
+ let x = 4.10
+ let y = 5.10
+ 
+ let suma = x + y
+ 
 
 /*: Question 2
 ### 2. Add an Int to a Double
 */
 // write your code here
-
+ 
+let x = 3
+let y = 4.67
+let otra = x + (y)
 
 /*: Question 3
 ### 3. Compare two Ints for equality
 */
 // write your code here
-
+ 
+2==4
 
 /*: Question 4
 ### 4. Compare two Doubles for equality
 */
 // write your code here
-
+ 
+4.10 == 3.34
 
 /*: Question 5
 ### 5. Compare an Int and a Double equality
 */
 // write your code here
 
+ 4 >= (10.35)
 
 /*: Question 6
 ### 6. Compare 3 and 3.5 for equality (NB what do you have to be mindful of in this case?)
 */
 // write your code here
 
+ 3==(3.5)
 
 /*: Question 7
 ### 7. Compare 3 and 3.5 for equality and 6.1 and 7 for inequality
 */
 // write your code here
-
+ 
+3==(3.5)
+(6.1) != 7
 
 /*: Question 8
 ### 8. Write code expressing "if a less than b or x is greater than or equal to y"
 */
 // write your code here
-
+ 
+if a<b || x>=y
+ 
 
 /*: Question 9
 ### 9. Write a function which returns the sum of three Ints
 */
 // write your code here
 
+ func addInts (numb1: Int, numb2: Int, numb3: Int){
+ let x = 1
+ let y = 2
+ let z = 3
+ print (x + y + z)
+ }
 
 /*: Question 10
 ### 10. Write a function average_i which returns the average of three Ints as an Int
 */
 // write your code here
 
+ func average(numb1: Int, numb2: Int, numb3: Int){
+ var sum = 0
+ var ave: Int = Int (sum) / Int
+ return ave
+ }
 
 /*: Question 11
 ### 11. Use the return value of the function written for Question 10 in a boolean expression
 */
 // write your code here
 
+ var isAve = true
 
 /*: Question 12
 ### 12. Write a function average_f which returns the average of three Ints as a Float
 */
 // write your code here
 
-
+ func average(numb1: Int, numb2: Int, numb3: Int){
+ var sum = 0
+ var ave: Int = Int (sum) / (float)
+ return ave
+ }
 /*: Question 13
 ### 13. Call average_f with values 1, 3, and 5 and test if the result equals 3.0
 */
 // write your code here
 
-
+ func average(numbers: Int...) -> Double {
+ var x = 1
+ var y = 3
+ var z = 5
+ var sum = x + y + z
+ for number in numbers {
+ sum += number
+ }
+ var  ave : Double = Double(sum) / Double(numbers.count)
+ return ave
+ }
 /*: Question 14
 ### 14. Call average_i with values 1, 3, and 5 and test if the result is both greater than 1 and less than 5.
 */
 // write your code here
 
+average( ave >1<5) 
+ 
 */
