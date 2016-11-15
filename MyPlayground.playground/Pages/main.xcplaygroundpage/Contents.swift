fcb45b80724b19478142dc81ea025d195da43e2d
HGClarke <hollandglenclarke@yahoo.com>
2016-11-15 02:11:14 -0500

    Added Functions

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,30 +15,40 @@
 ### 1. Add two Doubles
 */
 // write your code here
-
-
+var num_1: Double = 2.0
+var num_2: Double = 4.5
 /*: Question 2
 ### 2. Add an Int to a Double
 */
 // write your code here
-
+ var x = 1
+ var sum = num_1 + Double(x)
 
 /*: Question 3
 ### 3. Compare two Ints for equality
 */
 // write your code here
 
-
+var y = 1
+ if x != {
+ print("Hi")
+ }
+ 
 /*: Question 4
 ### 4. Compare two Doubles for equality
 */
 // write your code here
-
+if num_1 == num_2{
+ print("Equal")
+ }
 
 /*: Question 5
 ### 5. Compare an Int and a Double equality
 */
 // write your code here
+ if x == Int(num_1){
+ print("Equal")
+ }
 
 
 /*: Question 6
@@ -46,29 +56,35 @@
 */
 // write your code here
 
-
+if Double(3) == 3.5
 /*: Question 7
 ### 7. Compare 3 and 3.5 for equality and 6.1 and 7 for inequality
 */
 // write your code here
-
+if Double(3) ==  3.5
+if 6.1 != Double(7)
 
 /*: Question 8
 ### 8. Write code expressing "if a less than b or x is greater than or equal to y"
 */
 // write your code here
 
-
+if a < b }} x >= y
 /*: Question 9
 ### 9. Write a function which returns the sum of three Ints
 */
 // write your code here
-
+func sum(number: Int , number2: Int, number3: Int) -> Int{
+ return (number + number2 + number3)
+ }
 
 /*: Question 10
 ### 10. Write a function average_i which returns the average of three Ints as an Int
 */
 // write your code here
+ 
+func average_i(number1: Int, number2: Int, number3: Int) -> Int{
+ return ((number1 + number2+ number3) / 3)
 
 
 /*: Question 11
@@ -76,6 +92,7 @@
 */
 // write your code here
 
+ func
 
 /*: Question 12
 ### 12. Write a function average_f which returns the average of three Ints as a Float
