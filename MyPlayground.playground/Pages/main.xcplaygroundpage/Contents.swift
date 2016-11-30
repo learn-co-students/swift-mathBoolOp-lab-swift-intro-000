f7e11ecc5667ff67fa8c8619847116ce719df5ec
jacquedes <jacquedes@gmail.com>
2016-11-29 19:43:57 -0600

    Solutions
    Problem solutions

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,79 +14,105 @@
 /*: Question 1
 ### 1. Add two Doubles
 */
-// write your code here
+let a: Double = 2.5
+let b: Double = 4.5
+ 
+let c = a + b // 7
 
 
 /*: Question 2
 ### 2. Add an Int to a Double
 */
-// write your code here
+let d: Int = 7
+let e: Double = 4.7
+ 
+let f = Int(d) + Double(e) // 11.7
 
 
 /*: Question 3
 ### 3. Compare two Ints for equality
 */
-// write your code here
-
+let g: Int = 4
+let h: Int = 1
+ 
+g >= h // true
 
 /*: Question 4
 ### 4. Compare two Doubles for equality
 */
-// write your code here
+let i: Double = 5.6
+let j: Double = 7.8
+ 
+i <= j // true
 
 
 /*: Question 5
 ### 5. Compare an Int and a Double equality
 */
-// write your code here
+let k: Int = 6
+let l: Double = 9.2
 
-
+Int(k) != Double(l) // true
 /*: Question 6
 ### 6. Compare 3 and 3.5 for equality (NB what do you have to be mindful of in this case?)
 */
-// write your code here
+let m: Int = 3
+let n: Double = 3.5
+ 
+Int(m) <= Double(n) // true
 
 
 /*: Question 7
 ### 7. Compare 3 and 3.5 for equality and 6.1 and 7 for inequality
 */
-// write your code here
-
+let o: Int = 3
+let p: Double = 3.5
+let q: Double = 6.1
+let r: Int = 7
+ 
+Int(o) < Double(p) && Double(q) >= Int(r) // true && false
 
 /*: Question 8
 ### 8. Write code expressing "if a less than b or x is greater than or equal to y"
 */
-// write your code here
+if (a < b) || (x >= y)
 
 
 /*: Question 9
 ### 9. Write a function which returns the sum of three Ints
 */
-// write your code here
+func sum(a: Int, b: Int, c: Int) -> Int {
+    return a + b + c
+}
+ 
+sum(a: 7, b: 13, c:5) //25
 
 
 /*: Question 10
 ### 10. Write a function average_i which returns the average of three Ints as an Int
 */
-// write your code here
+func average_i(v: Int, w: Int, x: Int) -> Int {
+    return (v + w + x)/3
+ }
 
-
+let returnValue = average_i(v: 20, w: 10, x:30) // 20
 /*: Question 11
 ### 11. Use the return value of the function written for Question 10 in a boolean expression
 */
-// write your code here
+ 
+
 
 
 /*: Question 12
 ### 12. Write a function average_f which returns the average of three Ints as a Float
 */
-// write your code here
-
-
+func average_f(a: Int, b: Int, c: Int) -> Float {
+    return Float ((a + b + c)/3)
+}
 /*: Question 13
 ### 13. Call average_f with values 1, 3, and 5 and test if the result equals 3.0
 */
-// write your code here
+average_f = (a: 1, b:3, c:5)
 
 
 /*: Question 14
