af94432933a59acf7cb4374a38fb366d1499dece
Thomas Houtart <Thomas.Houtart@hotmail.com>
2017-01-01 15:34:17 -0500

    Boolean Lab

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,83 +15,96 @@
 ### 1. Add two Doubles
 */
 // write your code here
-
-
+*/
+var double1: Double = 1
+var double2: Double = 1.2
 /*: Question 2
 ### 2. Add an Int to a Double
 */
 // write your code here
-
-
+var Int1: Int = 6
+var Int2Double = Double(Int1)
 /*: Question 3
 ### 3. Compare two Ints for equality
 */
 // write your code here
-
-
+var Int2: Int = 8
+Int1 == Int2
 /*: Question 4
 ### 4. Compare two Doubles for equality
 */
 // write your code here
-
+double1 == double2
 
 /*: Question 5
 ### 5. Compare an Int and a Double equality
 */
 // write your code here
-
+//Gives an error since you can't compare values of different type
+//Int1 == Int2Double
 
 /*: Question 6
 ### 6. Compare 3 and 3.5 for equality (NB what do you have to be mindful of in this case?)
 */
 // write your code here
-
+3 == 3.5
 
 /*: Question 7
 ### 7. Compare 3 and 3.5 for equality and 6.1 and 7 for inequality
 */
 // write your code here
-
+3 == 3.5
+6.1 != 7
 
 /*: Question 8
 ### 8. Write code expressing "if a less than b or x is greater than or equal to y"
 */
 // write your code here
+let a = 1
+let b = 2
+let x = 3
+let y = 4
 
-
+a < b || x >= y
 /*: Question 9
 ### 9. Write a function which returns the sum of three Ints
 */
 // write your code here
-
-
+func Sum(Int1: Int, Int2: Int, Int3: Int) -> Int{
+    return Int1 + Int2 + Int3
+}
+Sum(Int1: a, Int2: b, Int3: x)
 /*: Question 10
 ### 10. Write a function average_i which returns the average of three Ints as an Int
 */
 // write your code here
-
-
+func average_i(Int1: Int, Int2: Int, Int3: Int) -> Int {
+    return Int((Int1 + Int2 + Int3)/3)
+}
+average_i(Int1: a, Int2: b, Int3: x)
 /*: Question 11
 ### 11. Use the return value of the function written for Question 10 in a boolean expression
 */
 // write your code here
-
-
+var Rvalue=average_i(Int1: a, Int2: b, Int3: x)
+Rvalue == 2 && true
 /*: Question 12
 ### 12. Write a function average_f which returns the average of three Ints as a Float
 */
 // write your code here
-
-
+func average_f(Int1: Int, Int2: Int, Int3: Int) -> Float{
+    return Float((Int1 + Int2 + Int3)/3)
+}
+average_f(Int1: a, Int2: b, Int3: x)
 /*: Question 13
 ### 13. Call average_f with values 1, 3, and 5 and test if the result equals 3.0
 */
 // write your code here
-
+average_f(Int1: 1, Int2: 3, Int3: 5)
 
 /*: Question 14
 ### 14. Call average_i with values 1, 3, and 5 and test if the result is both greater than 1 and less than 5.
 */
 // write your code here
+average_i(Int1: 1, Int2: 3, Int3: 5) > 1 && average_i(Int1: 1, Int2: 3, Int3: 5) < 5
 
-*/
