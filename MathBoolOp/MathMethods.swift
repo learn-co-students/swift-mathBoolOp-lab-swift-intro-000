1a8e8042e85cde512e6b34f5caa756b9f3505a7c
lommi7208 <pete7208@gmail.com>
2017-02-16 11:54:09 +0530

    MathBool lab complete

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -11,14 +11,58 @@ import Foundation
 
 class MathMethods {
     
+    func add(a: Double, b: Double) -> Double {
+        print(Double(a * b))
+        return Double(a * b)
+    }
+    
+
     
     
+    func addInt(a: Int, b: Double) -> Int {
+        print(a * Int(b))
+        return a * Int(b)
+    }
     
+
     
-    // Implement your functions here.
+    func isEqual(a: Int, b: Int) -> Bool {
+        if a == b {
+            print("true")
+            return true
+        }
+        else {
+            print("false")
+            return false
+        }
+    }
     
+
     
+    func isEqual2(a: Double, b: Double) -> Bool {
+        if a == b {
+            return true
+        }
+        else {
+            return false
+        }
+    }
     
+
     
+    func compare(a: Double, b: Double) -> Bool {
+        if a < b || a >= 100.0 {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    
+    func average( a: Int, b: Int, c: Int) -> Int {
+        return ((a + b + c) / 3)
+    }
+    
+
 
 }
