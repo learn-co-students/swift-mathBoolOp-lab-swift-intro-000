c21ddcfacfdca1bf0458ec8646b94bdf3c913666
Richard Garrison <threeduser@hotmail.com>
2017-02-20 22:09:14 -0700

    Complete problems

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -17,8 +17,53 @@ class MathMethods {
     
     // Implement your functions here.
     
+    func add(a: Double, b: Double) -> Double{
+        return a + b
+    }
     
     
+    func add(a: Int, b: Double) -> Int {
+        return a + Int(b)
+    }
     
-
+    
+    func isEqual(a: Int, b: Int) -> Bool {
+        if a == b {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func isEqual(a: Double, b: Double) -> Bool {
+        if a == b {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func questionFive() -> String {
+        if 6.1 == 7 {
+            return "a"
+        }else if 6.1 == 7 {
+            return "b"
+        }else if 6.1 == 7 {
+            return "c"
+        }else {
+            return "d"
+        }
+        
+    }
+        func compare(a: Double, b: Double) -> Bool {
+            if a < b || a >= 100 {
+                return true
+            }else {
+                return false
+            }
+        }
+        
+        func average(a: Int, b: Int, c: Int) -> Int {
+            return (a + b + c) / 3
+        }
 }
