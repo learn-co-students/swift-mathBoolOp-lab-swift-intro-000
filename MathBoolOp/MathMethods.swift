e0f5807ae50ab8264f4e2a6eef4a0ff187911c53
hanooNam <jnam920329@gmail.com>
2017-03-03 16:35:34 -0500

    mathLab

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -10,15 +10,31 @@ import Foundation
 
 
 class MathMethods {
+    func add(a:Double, b:Double) -> Double {
+        return a+b
+    }
     
+    func addTypeConversion(a:Int, b:Double) -> Int{
+        return a+Int(b)
+    }
+    func isEqual(a :Int, b:Int)->Bool{
+        return a==b
+    }
+    func isEqualDouble(a :Double, b:Double)->Bool{
+        return a==b
     
-    
-    
-    
-    // Implement your functions here.
-    
-    
-    
-    
-
+    }
+    func questionFive() ->String{
+        return "a"
+    }
+    func compare(a :Double, b: Double) ->Bool{
+        if (a>b) || (a>Double(100)){
+            return true
+        }else{
+            return false
+        }
+    }
+    func average(a:Int, b:Int, c:Int) -> Int {
+        return (a+b+c)/3
+    }
 }
