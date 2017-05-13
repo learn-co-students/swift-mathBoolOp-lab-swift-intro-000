929d278e99ddaf49fcf00eed829097f46b66dad4
ZellisE82 <zellis82@gmail.com>
2017-05-13 16:03:28 -0700

    Lab work

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -15,10 +15,49 @@ class MathMethods {
     
     
     
-    // Implement your functions here.
-    
-    
-    
-    
+    func add(a:Double, b:Double)->Double{
+        return a + b
+    }
 
-}
+    
+    
+    func add1(a:Int, b:Double)-> Int {
+        return a + Int(b)
+    }
+    func isEqual(a:Int, b:Int)->Bool{
+        if a == b  {
+            return true } else {
+            return false
+        }
+            }
+    func isEqual1(a:Double, b:Double)->Bool {
+        if a == b {
+            return true } else {
+            return false
+        }
+        }
+    func compare(a:Double, b:Double)->Bool {
+        if a < b || a > 100 {
+            return true } else {
+            return false
+        }
+        }
+    func average(a:Int,b:Int,c:Int)->Int {
+        return (a + b + c) / 3
+    }
+    func questionFive()->String {
+        return "c"
+    }
+    func compare1(a:Double, b:Double)->Bool {
+        if a < b || a >= 100 {
+            return true } else {
+            return false }
+        }
+    func average1(a:Int,b:Int,c:Int)->Int{
+        return ((a + b + c)/3)
+    }
+        }
+
+
+
+
