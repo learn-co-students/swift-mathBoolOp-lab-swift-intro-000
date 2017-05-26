f3d7c73827afa8a0d0f4d8ca31aa62d18f30407d
davidtspf <david.tannenbaum@gmail.com>
2017-05-26 15:31:49 -0400

    Answered with another omission

--- MathBoolOpTests/MathBoolOpTests.swift
+++ MathBoolOpTests/MathBoolOpTests.swift
@@ -29,13 +29,6 @@ class MathBoolOpTests: XCTestCase {
         XCTAssertEqual(result2, -3, "0 + -3 is equal to -3.")
     }
     
-    // #2
-    func testAddWithIntAndDouble() {
-        let result = test.add(a: 5, b: 25.5)
-        let result2 = test.add(a: 1, b: 99.9)
-        XCTAssertEqual(result, 30, "25.5 + 5 when 25.5 is converted to an Int should equal 30.")
-        XCTAssertEqual(result2, 100, "1 + 99.9 when 99.9 is converted to an Int should equal 100.")
-    }
     
     // #3
     func testIsEqual() {
