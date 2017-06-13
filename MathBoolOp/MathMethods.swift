23f88102bbdcf5903c97886806c11a6cf45b392f
Vaibhav Bhat <vaibhavrdbhat@gmail.com>
2017-06-13 00:42:49 -0700

    mathbool lab

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -15,8 +15,28 @@ class MathMethods {
     
     
     
-    // Implement your functions here.
+    func add(a: Double, b: Double) -> Double {
+        return a + b
+    }
     
+    func add(a: Int, b: Double) -> Int {
+        return a + Int(b)
+    }
+    func isEqual(a: Int, b: Int) -> Bool {
+        return a == b
+    }
+    func isEqual(a: Double, b: Double) -> Bool {
+        return a == b
+    }
+    func questionFive() -> String {
+        return "d"
+    }
+    func compare(a: Double, b: Double) -> Bool {
+        return a < b || a >= 100
+    }
+    func average(a: Int, b: Int, c: Int) -> Int {
+        return (a + b + c)/3
+    }
     
     
     
