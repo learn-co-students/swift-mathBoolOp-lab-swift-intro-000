173e29a78810c39c081192b1d48e252cf3873f8e
kjdavis2423@gmail.com <kingdavis2423@gmail.com>
2017-06-29 18:51:01 -0400

    Complete

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Jim Campagno. All rights reserved.
 //
 
+
 import Foundation
 
 
@@ -17,8 +18,53 @@ class MathMethods {
     
     // Implement your functions here.
     
+    func add(a: Double, b: Double) -> Double{
+        return a + b
+    }
     
     
+    func add(a: Int, b: Double) -> Int {
+        return a + Int(b)
+    }
     
-
+    
+    func isEqual(a: Int, b: Int) -> Bool {
+        if a == b {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func isEqual(a: Double, b: Double) -> Bool {
+        if a == b {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func questionFive() -> String {
+        if 6.1 == 7 {
+            return "a"
+        }else if 6.1 == 7 {
+            return "b"
+        }else if 6.1 == 7 {
+            return "c"
+        }else {
+            return "d"
+        }
+        
+    }
+    func compare(a: Double, b: Double) -> Bool {
+        if a < b || a >= 100 {
+            return true
+        }else {
+            return false
+        }
+    }
+    
+    func average(a: Int, b: Int, c: Int) -> Int {
+        return (a + b + c) / 3
+    }
 }
