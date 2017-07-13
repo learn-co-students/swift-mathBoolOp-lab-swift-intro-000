43e16d9794c758a5ed7bef182ef58ce6cff0a592
dgmalot <dgmalot@gmail.com>
2017-07-13 16:20:15 -0400

    Completed lab, except question 5, which is not in the file.

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -11,14 +11,37 @@ import Foundation
 
 class MathMethods {
     
-    
-    
-    
-    
     // Implement your functions here.
     
+    // Question 1
+    func add(a: Double, b: Double) -> Double {
+        return a + b
+    }
     
+    // Question 2
+    func add(a: Int, b: Double) -> Int {
+        return a + Int(b)
+    }
     
+    // Question 3
+    func isEqual(a: Int, b: Int) -> Bool {
+        return a == b
+    }
     
-
+    // Question 4
+    func isEqual(a: Double, b: Double) -> Bool {
+        return a == b
+    }
+    
+    // Question 5 is missing from this file
+    
+    // Question 6
+    func compare(a: Double, b: Double) -> Bool {
+        return (a < b || a >= 100.0)
+    }
+    
+    // Question 7
+    func average(a: Int, b: Int, c: Int) -> Int {
+        return (a + b + c)/3
+    }
 }
