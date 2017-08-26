c8193a5360e2822454f746b0ec3c197906847245
AsifAliJohari <asifjohari2013@gmail.com>
2017-08-26 07:15:55 -0700

    Add files via upload

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -11,14 +11,35 @@ import Foundation
 
 class MathMethods {
     
-    
-    
-    
-    
     // Implement your functions here.
-    
-    
-    
-    
+    //Q.1
+    func add(a:Double,b:Double) -> Double {
+        return a+b
+    }
+    //Q.2
+    func add(a:Int,b:Double) -> Int {
+        return a+Int(b);
+    }
+    //Q.3
+    func isEqual(a:Int,b:Int) -> Bool {
+        return (a==b)
+    }
+    //Q.4
+    func isEqual(a:Double,b:Double) -> Bool {
+        return (a==b)
+    }
+    //Q.5
+    func questionFive() -> String {
+        return "d"
+    }
 
+    //Q.6
+    func compare(a:Double,b:Double) -> Bool {
+        return (a < b && a >= 100)
+    }
+    
+    //Q.7
+    func avarage(a:Int,b:Int,c:Int) -> Int {
+        return ((a+b+c)/3)
+    }
 }
