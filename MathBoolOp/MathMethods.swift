7f101d57a4258ce70032bcbcc560303933ef104f
OthmanAmoudi <red.fantastic@hotmail.com>
2017-09-15 18:18:41 +0300

    solved

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -16,9 +16,35 @@ class MathMethods {
     
     
     // Implement your functions here.
+    func add(a:Int,b:Int)->Int{
+        return a + b
+    }
     
+    func add(a:Int,b:Double)->Int{
+        return a + Int(b)
+    }
     
+    func isEqual(a:Double,b:Double)-> Bool{
+        if a>b{
+            return true
+        }
+        return false
+    }
     
+    func questionFive() -> String {
+        return "d"
+    }
     
+    func compare(a:Double,b:Double)->Bool{
+        if (a < b || a >= 100 ){
+            return true
+        }
+        return false
+    }
+    
+    func average(a:Int,b:Int,c:Int) ->Int{
+        let average = (a+b+c)/3
+        return average
+    }
 
 }
