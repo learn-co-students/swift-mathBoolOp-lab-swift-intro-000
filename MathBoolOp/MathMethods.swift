a2ddaaa3a7ddd43f10f4574c33cded3db33354e8
hohl <mhohl@rocketmail.com>
2017-10-18 16:06:22 -0700

    first attempt

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -17,8 +17,48 @@ class MathMethods {
     
     // Implement your functions here.
     
-    
-    
-    
+   func add(a:Double, b:Double) -> Double {
+      var sum:Double
+      sum = a + b
+         return(sum)
+   }
+   
+   func add(a:Int, b:Double) -> Int {
+      var sum:Int
+      sum = a + Int(b)
+      return(sum)
+   }
+   
+   func isEqual(a:Int, b:Int) -> Bool {
+      if a == b {
+         return true
+      } else {
+         return false
+      }
+   }
 
+   func isEqual(a:Double, b:Double) -> Bool {
+      if a == b {
+         return true
+      } else {
+         return false
+      }
+   }
+   
+   func questionFive() -> String {
+      return "d"
+   }
+   
+   func compare(a:Double,b:Double) -> Bool {
+      if a < b || a >= 100 {
+         return true
+      } else {
+         return false
+      }
+   }
+   
+   func average (a:Int, b:Int, c:Int) -> Int {
+      return (a+b+c)/3
+   }
+   
 }
