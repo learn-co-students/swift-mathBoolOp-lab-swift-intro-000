1a648fa563779ed15755d399fc3dac8abfa8bd90
Chuck Underwood <chillieguy@live.com>
2018-01-10 04:55:27 -0800

    Completed tasks to make tests pass

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -16,9 +16,34 @@ class MathMethods {
     
     
     // Implement your functions here.
+    func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
     
+    func add(a: Int, b: Double) -> Int {
+        return a + Int(b)
+    }
     
+    func isEqual(a: Int, b: Int) -> Bool {
+        return a == b ? true : false
+    }
     
+    func isEqual(a: Double, b: Double) -> Bool {
+        return a == b ? true : false
+    }
+    
+    func questionFive() -> String {
+        return "d"
+    }
+    
+    func compare(a: Double, b: Double) -> Bool {
+        return (a < b || a > 100.0) ? true : false
+    }
+    
+    func average(a: Int, b: Int, c: Int) -> Int {
+        return Int((a + b + c) / 3)
+        
+    }
     
 
 }
