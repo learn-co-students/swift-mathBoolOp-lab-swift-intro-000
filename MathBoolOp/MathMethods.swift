07c3ceea2439f99e783a8b37e81be170a0ffe662
Jon <jbvaldesuso@gmail.com>
2018-03-04 16:42:19 -0800

    done

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -17,8 +17,43 @@ class MathMethods {
     
     // Implement your functions here.
     
+    // 1
+    func add(a: Double, b: Double) -> Double {
+        return a + b
+    }
     
+    // 2
+    func add(a: Int, b: Double) -> Int {
+        return a + Int(b)
+    }
     
+    // 3
+    func isEqual(a: Int, b: Int) -> Bool {
+        return a == b
+    }
     
-
+    // 4
+    func isEqual(a: Double, b: Double) -> Bool {
+        return a == b
+    }
+    
+    // 5
+    func questionFive() -> String {
+        return "d" // This would be a problem when comparing variables with different types. If it is just numbers we can compare both numbers without a problem
+    }
+    
+    // 6
+    func compare(a: Double, b: Double) -> Bool {
+        if a < b || a >= 100 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    // 7
+    func average(a: Int, b: Int, c: Int) -> Int {
+        return (a + b + c) / 3
+    }
+    
 }
