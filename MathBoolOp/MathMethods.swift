5eeebbc53aed28217d3711c3b29f61a7760f0dec
vinodkalpka <vinod.wst@gmail.com>
2018-03-07 10:08:35 +0530

    functions but not completed
    functions but not completed

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -12,12 +12,57 @@ import Foundation
 class MathMethods {
     
     
+    func add(a:Int, b:Int) ->Int{
+        let c:Int =  a + b
+        return c
+    }
+    func add(a:Double, b:Double) ->Double{
+        let c:Double =  a + b
+        return c
+    }
+    func add(a:Int, b:Double) ->Int{
+        let c:Int =  a + Int(b)
+        return c
+    }
+    func isEqual(a:Int, b:Int) ->Bool{
+        if(a == b){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    func isEqual(a:Double, b:Double) ->Bool{
+        if(a == b){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+   
     
+    func questionFive() -> String{
+    return "test"
+    }
     
-    
+    func  compare(a:Int, b:Int) ->  Bool {
+        if(a >  b ){
+            return true
+        }
+        else if ( a >= 100){
+            return true
+        }
+        else{
+            return false
+        }
+    }
     // Implement your functions here.
     
-    
+    func average(a:Int,b:Int, c:Int) -> Int {
+        let c:Int =  (a + b +  c ) / 2
+        return c
+    }
     
     
 
