7e0f6bb93de66f8206414718c44620eae06b87a8
sammichthief <sammichthief@gmail.com>
2018-03-13 15:43:23 -0400

    Lab Completed

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -14,11 +14,62 @@ class MathMethods {
     
     
     
+   // 1
+    func add(a: Double, b: Double) -> Double {
+        return a + b
+    }
     
-    // Implement your functions here.
+    // 2
+    func add(a: Int, b: Double) -> Int {
+        return a + Int(b)
+    }
+    
+    // 3
+    func isEqual(a: Int, b: Int) -> Bool {
+        if a == b {
+            return true
+        } else {
+            return false
+        }
+    }
+    // 4
+    func isEqual(a: Double, b: Double) -> Bool {
+        if a == b {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    // 5
+    
+    func questionFive() -> String {
+        return "d"
+    }
+    
+    // 6
+    
+    func compare(a: Double, b: Double) -> Bool {
+        if a < b || a >= 100 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    // 7
+    
+    func average(a: Int, b: Int, c: Int) -> Int {
+        return (a + b + c) / 3
+    }
+    
+    // 8
     
     
     
     
 
 }
+
+
+
