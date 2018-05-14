a5efcef4ebf088219d84681fab17e241b56eaf2b
KevinJStafford <staffordkevinjames@gmail.com>
2018-05-14 12:52:07 -0400

    First Test completed

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -11,14 +11,45 @@ import Foundation
 
 class MathMethods {
     
+    func add(a: Double, b: Double) -> Double {
+        return a + b
+    }
     
+    func add(a: Int, b: Double) -> Int {
+        let sum = a + Int(b)
+        return sum
+    }
     
+    func isEqual(a: Int, b: Int) -> Bool {
+        if a == b {
+            return true
+        } else {
+            return false
+        }
+    }
     
+    func isEqual(a: Double, b: Double) -> Bool {
+        if a==b {
+            return true
+        } else {
+            return false
+        }
+    }
     
-    // Implement your functions here.
+    func questionFive() -> String {
+        let answer = "d"
+        return answer
+    }
     
+    func compare(a: Double, b: Double) -> Bool {
+        if a < b || a >= 100 {
+            return true
+        } else {
+            return false
+        }
+    }
     
-    
-    
-
+    func average(a: Int, b: Int, c: Int) -> Int {
+        return (a + b + c) / 3
+    }
 }
