bf9be405d8562cf46b64e9ce56e02478f8e71800
Jimmers0 <Bentleyx007@gmail.com>
2018-05-27 12:41:15 -0700

    created all the functions

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -10,12 +10,60 @@ import Foundation
 
 
 class MathMethods {
+  
+  // Implement your functions here.
     
+  func add(a: Double, b: Double) -> Double {
     
+    let result = a + b
     
+    return result
+  }
     
+  func add(a: Int, b: Double) -> Int {
     
-    // Implement your functions here.
+    let sum = a + Int(b)
+    
+    return sum
+  }
+  
+  func isEqual(a: Int, b: Int) -> Bool {
+    
+    let result = a == b
+    
+    return result
+  }
+  
+  func isEqual(a: Double, b: Double) -> Bool {
+    let result = a == b
+    
+    return result
+  }
+  
+  func questionFive() -> String {
+
+    return "d"
+  }
+  
+  func compare(a: Double, b: Double) -> Bool {
+    
+    if a < b || a >= 100 {
+      return true
+    } else {
+      return false
+    }
+    
+  }
+  
+  func average(a: Int, b: Int, c: Int) -> Int {
+    
+    let average = (a + b + c) / 3
+    
+    return average 
+  }
+  
+  
+  
     
     
     
