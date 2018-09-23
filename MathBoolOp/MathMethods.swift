8d6042cdc219b6e8a64635be6d98bf6757401418
mjanoudy <muaawiaj1991@hotmail.com>
2018-09-23 15:44:56 +0300

    Completed Bolean Lab, tests passed but run local test light will not turn on

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -11,14 +11,45 @@ import Foundation
 
 class MathMethods {
     
+    func addAB (a: Double, b: Double) -> Double {
+        return a + b
+    }
     
+    func addAB2 (a: Int, b: Double) -> Int {
+        return a + Int(b)
+    }
     
+    func isEqual (a: Int, b: Int) -> Bool {
+        if a == b {
+            return true
+        } else {
+            return false
+        }
+    }
     
+    func isEqualDouble (a: Double, b: Double) -> Bool {
+        if a==b {
+            return true
+        } else {
+            return false
+        }
+    }
     
-    // Implement your functions here.
+    func questionFive () -> String {
+        return "d"
+    }
     
+    func compareAB(a: Double, b: Double) -> Bool {
+        if a < b || a >= 100.00 {
+            return true
+        } else {
+            return false
+        }
+    }
     
-    
+    func averageABC(a: Int, b: Int, c: Int) -> Int {
+        return (a + b + c)/3
+    }
     
 
 }
