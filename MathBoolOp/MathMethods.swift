ea6bd712a8d9b4bf446619489a310bc0b0917643
pmaurelia <pmaurelia@gmail.com>
2019-01-23 22:29:42 -0500

    final lab

--- MathBoolOp/MathMethods.swift
+++ MathBoolOp/MathMethods.swift
@@ -11,14 +11,61 @@ import Foundation
 
 class MathMethods {
     
+    func add(a: Double, b: Double) -> Double
+    {
+        return a + b
+    }
     
+    func add(a: Int, b: Double) -> Int
+    {
+        return a + Int(b)
+    }
     
+    func isEqual(a: Int, b: Int) -> Bool
+    {
+        return a==b
+    }
     
+    func isEqual(a: Double, b: Double) -> Bool
+    {
+        if a==b
+        {
+            return true
+        }
+        else
+        {
+            return false
+        }
+    }
     
-    // Implement your functions here.
+    func questionFive() -> String
+    {
+        let a = "We can't compare an Int against a Double. When making an equality check, bot values need to be of the same type"
+        let b = "We can compare both number without a problem"
+        let c = "We can compare both numbers if we convert one type to be the same as the other."
+        let d = "both a & c"
+        
+      
+        return "d"
+    }
     
-    
-    
-    
+    func compare(a: Double, b: Double) -> Bool
+    {
+        if a < b || a >= 100
+        {
+            return true
+        }
+        else
+        {
+            return false
+        }
+    }
 
+    func average(a: Int, b:Int, c:Int) -> Int
+    {
+        let total = a + b + c
+        let avg = total/3
+        
+        return avg
+    }
 }
